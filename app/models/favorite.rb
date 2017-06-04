887cc0ff2b19339eb80a95233ec620154b09f6e2
Stephen Flowers <sflower0@chicagobooth.edu>
2017-06-04 18:34:06 -0500

    favorites have validation
    good

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,8 @@
 class Favorite < ApplicationRecord
+
+  validates :user_id, :presence => true
+  validates :lesson_id, :presence => true, :uniqueness => {:scope => :user_id, :message => "You must really like this lesson. It's already in your favorites."}
+
   belongs_to :user
   belongs_to :lesson
 end
