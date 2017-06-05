84e385291a22fb528537d86cafe091b89ac8b1b6
Stephen Flowers <sflower0@chicagobooth.edu>
2017-06-04 23:22:26 -0500

    enrollments option added to user edit page

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,7 @@
 class Enrollment < ApplicationRecord
+
+validates :user_id, :presence => true, :uniqueness => {:scope => :user_id, :message => "error. You're already listed as being enrolled."}
+
   belongs_to :user
   belongs_to :school
 end
